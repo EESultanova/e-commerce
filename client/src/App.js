d8a273ce98aa7bd3314d033390e843aa583dfaa0
Dmitry Bystrov <dmitr_bystrov@mail.ru>
2021-04-08 17:05:07 +0300

    Change

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
-// import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 
 
 function App() {
