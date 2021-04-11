f3f460f6ab8f50312cc64f0a37fde165750dddf4
Dmitry Bystrov <dmitr_bystrov@mail.ru>
2021-04-11 15:47:03 +0300

    pics fixed

--- client/src/components/ProfileSettings/ProfileSettings.js
+++ client/src/components/ProfileSettings/ProfileSettings.js
@@ -1,8 +1,12 @@
 import { useSelector } from "react-redux";
+import { API_URL } from '../../config'
+import avatarLogo from '../../assets/avatar.svg';
 
 const ProfileSettings = () => {
-  const user = useSelector(state => state.user)
-  
+
+  const currentUser = useSelector(state => state.user);
+  const avatar = currentUser.avatar ? `${API_URL + currentUser.avatar}` : avatarLogo;
+
   return ( 
     <div class="card">
       <div class="card-body">
@@ -55,7 +59,7 @@ const ProfileSettings = () => {
 
      	</div>
      	<div class="col-md">
-     		<img src="images/avatars/avatar1.jpg" class="img-md rounded-circle border" alt=""/>
+     		<img src={avatar} class="img-md rounded-circle border" alt=""/>
      	</div>
       </form>
       </div>
