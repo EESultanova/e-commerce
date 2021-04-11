ccf60cac35d190a500e09775ca8ac8c2b85f1337
Artem <artp956@gmail.com>
2021-04-11 11:30:46 +0300

    react notifications

--- client/src/components/GoodDetails/GoodDetails.jsx
+++ client/src/components/GoodDetails/GoodDetails.jsx
@@ -1,8 +1,13 @@
-import { useEffect} from "react"
+import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { useParams } from "react-router"
 import { addGoodToCart, deleteGoodFromCart } from "../../redux/actionCreators/cartAC"
 import { getGoodDetailsFromServer } from "../../redux/actionCreators/goodAC"
+
+import { store } from 'react-notifications-component';
+
+import 'animate.css'
+import 'react-notifications-component/dist/theme.css'
 
 const GoodDetails = () => {
 
@@ -23,93 +28,178 @@ const GoodDetails = () => {
   cart.find(x => x._id === good._id)
   console.log(cart.find(x => x._id === good._id))
 
+  function NotifyAdd() {
+    return (
+      <div className="bg-primary text-white rounded" style={{ width: 200 }}>
+        <h6>{good.name} was added to your cart!</h6>
+      </div>
+    )
+  }
+
+  function NotifyRemove() {
+    return (
+      <div className="bg-secondary text-white rounded" style={{ width: 200 }}>
+        <h6>{good.name} was removed from your cart!</h6>
+      </div>
+    )
+  }
+
   return (
     <div className="row">
-          <aside className="col-md-6">
-      <div className="card">
-      <article className="gallery-wrap">
-        {good.photo &&
-          <>
-            <div className="img-big-wrap mt-5">
-              <div> <a href="/"><img src={good.photo} alt="" /></a></div>
-            </div>
-            <div className="thumbs-wrap">
-              {good.photo.length ? good.photo.map((photo, indx) => {
-                return (
-                  <a key={indx} href="/" className="item-thumb"> <img src={photo} alt="" /></a>
-                )
-              })
-              : ''
+      <aside className="col-md-6">
+        <div className="card">
+          <article className="gallery-wrap">
+            {good.photo &&
+              <>
+                <div className="img-big-wrap mt-5">
+                  <div> <a href="/"><img src={good.photo} alt="" /></a></div>
+                </div>
+                <div className="thumbs-wrap">
+                  {good.photo.length ? good.photo.map((photo, indx) => {
+                    return (
+                      <a key={indx} href="/" className="item-thumb"> <img src={photo} alt="" /></a>
+                    )
+                  })
+                    : ''
+                  }
+                </div>
+              </>
+            }
+          </article>
+        </div>
+      </aside>
+      <main className="col-md-6">
+        <article className="product-info-aside">
+
+          <h2 className="title mt-3">{good.name}</h2>
+
+          <div className="rating-wrap my-3">
+            <ul className="rating-stars">
+              <li style={{ width: 80 }} className="stars-active">
+                <i className="fa fa-star"></i> <i className="fa fa-star"></i>
+                <i className="fa fa-star"></i> <i className="fa fa-star"></i>
+                <i className="fa fa-star"></i>
+              </li>
+              <li>
+                <i className="fa fa-star"></i> <i className="fa fa-star"></i>
+                <i className="fa fa-star"></i> <i className="fa fa-star"></i>
+                <i className="fa fa-star"></i>
+              </li>
+            </ul>
+            <small className="label-rating text-muted">132 reviews</small>
+            <small className="label-rating text-success"> <i className="fa fa-clipboard-check"></i> 154 orders </small>
+          </div>
+
+          <div className="mb-3">
+            <var className="price h4">{good.price} $</var>
+          </div>
+
+          <p>{good.description}</p>
+
+
+          <dl className="row">
+            <dt className="col-sm-3">Manufacturer</dt>
+            <dd className="col-sm-9"><a href="/">Great textile Ltd.</a></dd>
+
+            <dt className="col-sm-3">Article number</dt>
+            <dd className="col-sm-9">596 065</dd>
+
+            <dt className="col-sm-3">Guarantee</dt>
+            <dd className="col-sm-9">2 year</dd>
+
+            <dt className="col-sm-3">Delivery time</dt>
+            <dd className="col-sm-9">3-4 days</dd>
+
+            <dt className="col-sm-3">Availabilty</dt>
+            <dd className="col-sm-9">in Stock</dd>
+          </dl>
+
+          <div className="form-row  mt-5">
+            <div className="form-group col-md">
+              {(inCart === true) ?
+                <button onClick={() => {
+                  // store.addNotification({
+                  //   // content: NotifyRemove,
+                  //   message: `${good.name} was removed from your cart!`,
+                  //   type: 'default',
+                  //   container: 'bottom-right',
+                  //   insert: 'bottom',
+                  //   animationIn: ['animated', 'fadeIn'],
+                  //   animationOut: ['animated', 'fadeOut'],
+
+                  //   dismiss: {
+                  //     duration: 2000,
+                  //     showIcon: true,
+                  //   },
+                  //   width: 200,
+
+                  // })
+                  store.addNotification({
+                    content: NotifyRemove,
+                    message: `${good.name} was removed from your cart!`,
+                    type: 'default',
+                    container: 'bottom-right',
+                    insert: 'bottom',
+                    animationIn: ['animated', 'fadeIn'],
+                    animationOut: ['animated', 'fadeOut'],
+
+                    dismiss: {
+                      duration: 2000,
+                    },
+                    width: 200,
+
+                  })
+                  dispatch(deleteGoodFromCart(good._id))
+                }} type="button" class="btn btn-secondary">Remove from cart</button>
+                :
+                <button onClick={() => {
+                  // store.addNotification({
+                  //   // content: NotifyAdd,
+                  //   // title: 'success',
+                  //   message: `${good.name} was added to your cart!`,
+                  //   type: 'warning',
+                  //   container: 'bottom-right',
+                  //   insert: 'bottom',
+                  //   animationIn: ['animated', 'fadeIn'],
+                  //   animationOut: ['animated', 'fadeOut'],
+
+                  //   dismiss: {
+                  //     duration: 2000,
+                  //     showIcon: true,
+                  //   },
+                  //   width: 200,
+
+                  // })
+                  store.addNotification({
+                    content: NotifyAdd,
+                    message: `${good.name} was added to your cart!`,
+                    type: 'warning',
+                    container: 'bottom-right',
+                    insert: 'succes',
+                    animationIn: ['animated', 'fadeIn'],
+                    animationOut: ['animated', 'fadeOut'],
+
+                    dismiss: {
+                      duration: 2000,
+                    },
+                    width: 200,
+
+                  })
+                  dispatch(addGoodToCart(good))
+                }} className="btn  btn-primary">
+                  <i className="fas fa-shopping-cart"></i> <span className="text">Add to cart</span>
+                </button>
               }
             </div>
-          </>
-        }
-      </article>
-      </div>
-          </aside>
-          <main className="col-md-6">
-      <article className="product-info-aside">
-
-      <h2 className="title mt-3">{good.name}</h2>
-
-      <div className="rating-wrap my-3">
-        <ul className="rating-stars">
-          <li style={{width: 80}} className="stars-active"> 
-            <i className="fa fa-star"></i> <i className="fa fa-star"></i> 
-            <i className="fa fa-star"></i> <i className="fa fa-star"></i> 
-            <i className="fa fa-star"></i> 
-          </li>
-          <li>
-            <i className="fa fa-star"></i> <i className="fa fa-star"></i> 
-            <i className="fa fa-star"></i> <i className="fa fa-star"></i> 
-            <i className="fa fa-star"></i> 
-          </li>
-        </ul>
-        <small className="label-rating text-muted">132 reviews</small>
-        <small className="label-rating text-success"> <i className="fa fa-clipboard-check"></i> 154 orders </small>
-      </div>
-
-      <div className="mb-3"> 
-        <var className="price h4">{good.price} $</var> 
-      </div>
-
-      <p>{good.description}</p>
-
-
-      <dl className="row">
-        <dt className="col-sm-3">Manufacturer</dt>
-        <dd className="col-sm-9"><a href="/">Great textile Ltd.</a></dd>
-
-        <dt className="col-sm-3">Article number</dt>
-        <dd className="col-sm-9">596 065</dd>
-
-        <dt className="col-sm-3">Guarantee</dt>
-        <dd className="col-sm-9">2 year</dd>
-
-        <dt className="col-sm-3">Delivery time</dt>
-        <dd className="col-sm-9">3-4 days</dd>
-
-        <dt className="col-sm-3">Availabilty</dt>
-        <dd className="col-sm-9">in Stock</dd>
-      </dl>
-
-        <div className="form-row  mt-5">
-          <div className="form-group col-md">
-            {(inCart === true) ?
-              <button onClick={() => dispatch(deleteGoodFromCart(good._id))} type="button" class="btn btn-secondary">Remove from cart</button>
-              :   
-              <button onClick={() => dispatch(addGoodToCart(good))} className="btn  btn-primary">
-              <i className="fas fa-shopping-cart"></i> <span className="text">Add to cart</span>
-            </button>
-            }
           </div>
-        </div>
 
-      </article>
-		</main>
-	</div>
-    
+        </article>
+      </main>
+    </div>
+
   )
 }
+
+
 
 export default GoodDetails
