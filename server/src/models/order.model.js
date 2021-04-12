fe0f7696f81d6f50a2abc77d4725c8fc2e78a0b1
Elya <esultanova96@gmail.com>
2021-04-12 15:32:51 +0300

    order

---
+++ server/src/models/order.model.js
@@ -0,0 +1,42 @@
+const { model, Schema } = require("mongoose");
+
+const orderSchema = new Schema({
+  fio: {
+    type: String,
+  },
+  address: {
+    type: String,
+ },
+  email: {
+    type: String,
+ },
+  phone: {
+    type: String,
+  },
+  card: {
+    type: Number,
+  },
+  cardName: {
+    type: String,
+  },
+  expMonth: {
+    type: String,
+  },
+  expYear: {
+    type: String,
+  },
+  cvv: {
+    type: Number,
+  },
+  cart: [],
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'Users',
+  },
+ 
+
+});
+
+const OrderModel = model('Orders', orderSchema);
+
+module.exports = OrderModel;
